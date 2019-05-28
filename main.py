6ed6e89cf26c869dda7ceea096e8f8411defcd6e
Sina <mohammad.h4s4ni@gmail.com>
2019-05-28 18:28:37 +0430

    ;

---
+++ main.py
@@ -0,0 +1,111 @@
+import cv2
+import tools
+import random
+import matplotlib.pyplot as plt
+from decimal import Decimal, ROUND_DOWN
+
+
+def plot_bar(labels, values, name):
+    plt.bar(labels, values)
+    plt.xlabel('size', fontsize=5)
+    plt.ylabel('distribution', fontsize=5)
+    # plt.xticks(index, label, fontsize=5, rotation=30)
+    plt.title('Micro')
+    plt.savefig('bar_plot' + str(name) + '.jpg')
+    plt.clf()
+
+# 21.5 * 20 um
+def main():
+    path = './test33.jpg'
+    measurement = 21.5 * 20 / 768
+
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(gray, 63, 255, cv2.THRESH_BINARY)
+
+    # find Contours
+    im2, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
+
+    counter = 0
+    dictionary = dict()
+    for i, v in enumerate(contours):
+
+        # if random.random() > .3:
+        #     continue
+
+        if hierarchy[0][i][3] != -1:
+            continue
+
+        x, y, w, h = cv2.boundingRect(v)
+
+        width = w * measurement
+        height = h * measurement
+        avg = (width + height) / 2
+        avg_int = int(avg)
+
+        if avg_int in dictionary:
+            l = list()
+            l.append(counter)
+            l.append((x, y))
+            l.append((w, h))
+            l.append((width, height))
+            l.append(avg)
+            dictionary[avg_int].append(l)
+        else:
+            l = list()
+            l.append(counter)
+            l.append((x, y))
+            l.append((w, h))
+            l.append((width, height))
+            l.append(avg)
+            dictionary[avg_int] = list()
+            dictionary[avg_int].append(l)
+        counter += 1
+
+    labels = list()
+    values = list()
+    labels.append(list())
+    values.append(list())
+    k = 1
+    for i in dictionary.keys():
+        d = dictionary[i]
+
+        count = len(d)
+        if count > 10000:
+            labels.append(list())
+            values.append(list())
+            d_temp = dict()
+            for j in d:
+                avg = j[4]
+                rounded = Decimal(avg).quantize(Decimal('.1'), rounding=ROUND_DOWN)
+                rounded = float(rounded)
+                if rounded in d_temp:
+                    d_temp[rounded].append(avg)
+                else:
+                    d_temp[rounded] = list()
+                    d_temp[rounded].append(avg)
+            for j in d_temp.keys():
+                labels[k].append(str(j))
+                values[k].append(len(d_temp[j]))
+            print(labels[k])
+            print(values[k])
+            plot_bar(labels[k], values[k], str(k))
+            k += 1
+
+        else:
+            labels[0].append(int(d[0][4]))
+            values[0].append(len(d))
+
+
+    plot_bar(labels[0], values[0], 'main')
+    tools.get_details(img, dictionary)
+    # cv2.imshow('s', img)
+    #
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # cv2.imwrite('/home/sina/Desktop/details.jpeg', img)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools.py
@@ -0,0 +1,59 @@
+import cv2
+
+
+def get_maximum_pixel_in_image(img):
+    maximum = 0
+    for i in img:
+        for j in i:
+            if j > maximum:
+                maximum = j
+    return maximum
+
+
+def get_average_pixel_in_image(img):
+    sum = 0
+    for i in img:
+        for j in i:
+            sum += j
+    denominator = img.shape[0] * img.shape[1]
+    average = int(sum / denominator)
+    return average
+
+
+def get_details(img, dictionary):
+    labels = list()
+    values = list()
+    for i in dictionary.keys():
+        d = dictionary[i]
+        img_tmp = img.copy()
+
+        labels.append(int(d[0][4]))
+        values.append(len(d))
+
+        for j in d:
+            counter = j[0]
+            x, y = j[1]
+            w, h = j[2]
+            avg = j[4]
+
+            img_tmp = cv2.rectangle(img_tmp, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+            # add number
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            position = (x, y)
+            fontScale = .3
+            fontColor = (255, 255, 255)
+            lineType = 1
+
+            img_tmp = cv2.putText(img_tmp, str(counter),
+                        position,
+                        font,
+                        fontScale,
+                        fontColor,
+                        lineType)
+            with open('details' + str(int(avg)) + '.xls', 'a') as f:
+                f.write(str(counter) + ', ' + str(avg))
+                f.write('\n')
+        img_name = int(d[0][4])
+        cv2.imwrite('details' + str(img_name) + '.jpg', img_tmp)
+
